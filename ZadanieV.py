a5c7c036e9c1ded40855161cf56d31010cc12a2b
Rafał Piechna <rpiechna@paradyz.com.pl>
2022-07-10 16:58:18 +0200

    gotowy skrypt

--- ZadanieV.py
+++ ZadanieV.py
@@ -3,3 +3,16 @@ from PIL.ExifTags import TAGS
 
 filename = '/Users/rafalpiechna/Downloads/test.bmp'
 image = Image.open(filename)
+info_dict = {
+    "Filename": image.filename,
+    "Image Size": image.size,
+    "Image Height": image.height,
+    "Image Width": image.width,
+    "Image Format": image.format,
+    "Image Mode": image.mode,
+    "Image is Animated": getattr(image, "is_animated", False),
+    "Frames in Image": getattr(image, "n_frames", 1)
+}
+
+for label,value in info_dict.items():
+    print(f"{label:25}: {value}")
